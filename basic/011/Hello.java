0bd5e7979eaded789139759e82dcbdf558fdc3b2
Ramesh Pandey <ramesh.pandey@tuni.fi>
2024-02-06 23:25:42 +0200

    ex 011 added

---
+++ basic/011/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * @author      Ramesh Pandey ramesh.pandey@tuni.fi
+ * @version     2024.02.06 (last modified)
+ * @since       17.0 (miminimum Java version)
+ */
+class HelloWorld {
+
+    /**
+     * Exercise: 1.1
+     * Write the Hello World program.
+     *
+     * @param  args  Command line arguments. Not used.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
