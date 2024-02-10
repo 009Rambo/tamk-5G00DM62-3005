28ab3da0e3b43a9970f453a9c1d2ee9a2a670dbc
Ramesh Pandey <ramesh.pandey@tuni.fi>
2024-02-10 06:16:36 +0200

    016 added

---
+++ basic/016/Grade.java
@@ -0,0 +1,59 @@
+/**
+ * Calculate Final Grade
+ *
+ * Calculate final grade based on weighted average mean.
+ * Use many different kinds of variables in the program.
+ * The Final grade can have any number of decimals in the output.
+ *
+ * [1] Weighted arithmetic mean
+ *
+ * @author Ramesh Pandey <ramesh.pandey@tuni.fi>
+ * @version 2024.0210
+ * @since 17.0
+ */
+
+class CalculateFinalGrade {
+    /**
+     * Calculates the final grade based on weighted average mean.
+     *
+     * @param ectsExam            ECTS points for the exam.
+     * @param gradeExam           Grade for the exam.
+     * @param ectsDesign          ECTS points for the design.
+     * @param gradeDesign         Grade for the design.
+     * @param ectsImplementation  ECTS points for the implementation.
+     * @param gradeImplementation Grade for the implementation.
+     * @return Final grade.
+     */
+    public static double calculateFinalGrade(int ectsExam, double gradeExam,
+                                             int ectsDesign, double gradeDesign,
+                                             int ectsImplementation, double gradeImplementation) {
+        double totalEcts = ectsExam + ectsDesign + ectsImplementation;
+        double weightedSum = (ectsExam * gradeExam) + (ectsDesign * gradeDesign) + (ectsImplementation * gradeImplementation);
+        return weightedSum / totalEcts;
+    }
+
+    /**
+     * Main method to execute the program.
+     *
+     * @param args Command line arguments (not used).
+     */
+    public static void main(String[] args) {
+        int ectsExam = 2;
+        double gradeExam = 2.0;
+        int ectsDesign = 1;
+        double gradeDesign = 4.0;
+        int ectsImplementation = 3;
+        double gradeImplementation = 4.0;
+
+
+        System.out.println("ECTS   Grade");
+        System.out.println("-----------------------------");
+        System.out.println("Exam            " + ectsExam + "      " + gradeExam);
+        System.out.println("Design          " + ectsDesign + "      " + gradeDesign);
+        System.out.println("Implementation  " + ectsImplementation + "      " + gradeImplementation);
+        System.out.println("-----------------------------");
+        System.out.println("Totals          " + (ectsExam + ectsDesign + ectsImplementation) + "      " + String.format("%.3f", calculateFinalGrade(ectsExam, gradeExam, ectsDesign, gradeDesign, ectsImplementation, gradeImplementation)));
+    }
+}
+
+
