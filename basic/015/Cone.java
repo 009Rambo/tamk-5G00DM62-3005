da16c56cf68fdf1280f12561b01ab630cb23aa4b
Ramesh Pandey <ramesh.pandey@tuni.fi>
2024-02-10 06:10:37 +0200

    015 added

---
+++ basic/015/Cone.java
@@ -0,0 +1,42 @@
+/**
+ * Calculate Cone
+ *
+ * Calculate triangular cone's volume from radius and height. Display result according to radius and height.
+ *
+ * volume = (1/3) * PI * r^2 * h
+ *
+ * [1] Triangular cone
+ *
+ * @author Ramesh Pandey <ramesh.pandey@tuni.fi>
+ * @version 2024.0210
+ * @since 17.0
+ */
+import java.lang.Math;
+
+class CalculateCone {
+    /**
+     * Calculates the volume of a triangular cone.
+     *
+     * @param radius Radius of the cone.
+     * @param height Height of the cone.
+     * @return Volume of the cone.
+     */
+    public static double calculateVolume(double radius, double height) {
+        return (1.0/3.0) * Math.PI * Math.pow(radius, 2) * height;
+    }
+
+    /**
+     * Main method to execute the program.
+     *
+     * @param args Command line arguments (not used).
+     */
+    public static void main(String[] args) {
+        double radius = 5; // Example radius
+        double height = 10; // Example height
+
+
+        System.out.println("radius: " + radius);
+        System.out.println("height: " + height);
+        System.out.printf("volume: %.2f\n", calculateVolume(radius, height));
+    }
+}
